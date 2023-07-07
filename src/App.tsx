1aea7a5a500c05a7b04d3da3a1cbba352b3b3d59
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-07-07 15:13:39 -0300

    add tailndcss in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import './styles/global.css'
+
 import Habit from "./components/Habit";
 
 function App() {
   return (
     <>
-      <Habit />
+      <Habit completed={5}/>
+      <Habit completed={4}/>
+      <Habit completed={3}/>
+      <Habit completed={2}/>
     </>
   );
 }
--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+interface HabitProps{
+    completed:number
+}
 
-export default function Habit() {
+export default function Habit(props:HabitProps) {
   return (
-    <div>Habit</div>
+    <div className='text-violet-700 h-10 w-10 m-2 flex justify-center items-center  bg-black'>{props.completed}</div>
   )
 }
