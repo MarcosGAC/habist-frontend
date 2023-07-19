2db54425a3494d5d8c5f0d68ba0b6e986797793f
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-07-19 13:29:37 -0300

    remove unused imports

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import * as Popover from "@radix-ui/react-popover";
 import ProgressBar from "./ProgressBar";
 import dayjs from "dayjs";
